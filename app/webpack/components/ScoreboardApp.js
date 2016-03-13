305110b1447b9260abfcac1c74b0476d32d9f511
Jared Norman <jared@creepywizard.com>
2016-03-13 15:34:05 -0700

    Add placeholder elements for the stuff that remains

--- app/webpack/components/ScoreboardApp.js
+++ app/webpack/components/ScoreboardApp.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-import LeagueIndicator from './LeagueIndicator';
+import LeagueIndicator from './scoreboard/LeagueIndicator';
+const nothing = () => <div />;
+const MessageArea = nothing;
+const AwayPlayer = nothing;
+const HomePlayer = nothing;
+const WarmUp = nothing;
 
 const App = () => (
   <div>
+    <MessageArea />
+    <AwayPlayer />
+    <HomePlayer />
     <LeagueIndicator />
+    <WarmUp />
   </div>
 );
 
